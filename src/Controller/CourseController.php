9d08b18f011757d2a07814dcb95e76e4feb3a989
Rémi Bernard <74245760+remib18@users.noreply.github.com>
2025-04-05 18:22:14 +0200

    Création des pages pour la visualisation des ressources d'un cours  (#3)
    * feat: adding the course page
    
    * feat: adding the members page and refactoring the pages of the course group
    
    * fix: correcting some issues and adding the message post type

---
+++ src/Controller/CourseController.php
@@ -0,0 +1,174 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class CourseController extends AbstractController
+{
+    #[Route('/course', name: 'app_course_courses')]
+    public function index(string $slug): Response
+    {
+        return $this->render('course/index.html.twig');
+    }
+    #[Route('/course/{slug}', name: 'app_course_course')]
+    public function course(string $slug): Response
+    {
+        return $this->render('course/course.html.twig', [
+            'courseName' => 'Cours de '.$slug,
+            'courseBase' => '/course/'.$slug,
+            'activities' => [
+                [
+                    'title' => 'Devoir xxx',
+                    'desc' => 'À rendre dans 3 jours',
+                    'target' => '/course/'.$slug.'/activity/xxx',
+                    'action' => 'Déposer'
+                ],
+                [
+                    'title' => 'Cours xxx',
+                    'desc' => 'Créé il y a 2 heures',
+                    'target' => '/course/'.$slug.'/activity/xxx',
+                    'action' => 'Voir'
+                ],
+                [
+                    'title' => 'Cours xxx',
+                    'desc' => 'Mis à jour hier',
+                    'target' => '/course/'.$slug.'/activity/xxx',
+                    'action' => 'Voir'
+                ],
+            ],
+            'pinnedRessources' => [
+                [
+                    'title' => 'Cours xxx',
+                    'userDesc' => 'Lire avant la prochaine séance',
+                    'target' => '/course/'.$slug.'/resource/xxx',
+                    'action' => 'Voir'
+                ],
+                [
+                    'title' => 'Devoir xxx',
+                    'userDesc' => 'À rendre pour la prochaine séance',
+                    'target' => '/course/'.$slug.'/resource/xxx',
+                    'action' => 'Déposer'
+                ],
+            ],
+            'categories' => [
+                [
+                    'title' => 'Général',
+                    'desc' => 'Informations générales sur l\'UE',
+                    'resources' => [
+                        [
+                            'title' => 'Mode de fonctionnement',
+                            'content' => 'Le cours est divisé en 3 parties : CM, TD et TP',
+                            'type' => 'message',
+                            'icon' => 'file-text',
+                            'severity' => 'info',
+                            'isPinned' => false,
+                        ],
+                        [
+                            'title' => 'S\'il vous plaît',
+                            'content' => 'Merci de ne pas envoyer de message à l\'enseignant en dehors des heures de cours',
+                            'type' => 'message',
+                            'icon' => 'file-text',
+                            'severity' => 'warning',
+                            'isPinned' => false,
+                        ],
+                    ],
+                ],
+                [
+                    'title' => 'Cours',
+                    'desc' => 'Ensemble des fichiers de cours',
+                    'resources' => [
+                        [
+                            'title' => 'CM 1 — xxx',
+                            'type' => 'document',
+                            'icon' => 'file-text',
+                            'isPinned' => false,
+                            'target' => '/course/'.$slug.'/resource/xxx',
+                        ],
+                        [
+                            'title' => 'CM 2 — xxx',
+                            'userDesc' => 'Se référer au site caniuse.com pour les navigateurs supportés',
+                            'type' => 'document',
+                            'icon' => 'file-archive',
+                            'isPinned' => false,
+                            'target' => '/course/'.$slug.'/resource/xxx',
+                        ],
+                        [
+                            'title' => 'CM 3 — xxx',
+                            'type' => 'document',
+                            'icon' => 'file-unknown',
+                            'isPinned' => true,
+                            'target' => '/course/'.$slug.'/resource/xxx',
+                        ],
+                    ],
+                ],
+                [
+                    'title' => 'Devoirs',
+                    'desc' => 'Ensemble des devoirs à rendre',
+                    'resources' => [
+                        [
+                            'title' => 'Devoir 1 — xxx',
+                            'userDesc' => 'Ne traiter que les exercices 1 et 2, nous en discuterons en TD',
+                            'type' => 'document-submission',
+                            'icon' => 'upload',
+                            'isPinned' => true,
+                            'target' => '/course/'.$slug.'/resource/xxx',
+                        ],
+                        [
+                            'title' => 'Devoir 2 — xxx',
+                            'type' => 'document-submission',
+                            'icon' => 'upload',
+                            'isPinned' => false,
+                            'target' => '/course/'.$slug.'/resource/xxx',
+                        ],
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    #[Route('/course/{slug}/members', name: 'app_course_members')]
+    public function courseMembers(string $slug): Response
+    {
+        return $this->render('course/members.html.twig', [
+            'courseName' => 'Cours de '.$slug,
+            'courseBase' => '/course/'.$slug,
+            'categories' => [
+                [
+                    'title' => 'Professeurs',
+                    'desc' => 'Ensemble des professeurs du cours',
+                    'members' => [
+                        [
+                            'firstname' => 'Jean',
+                            'lastname' => 'Dupont',
+                            'email' => 'jean@dupont.fr',
+                        ],
+                        [
+                            'firstname' => 'John',
+                            'lastname' => 'Doe',
+                            'email' => 'john@doe.fr',
+                        ]
+                    ],
+                ],
+                [
+                    'title' => 'Étudiants',
+                    'desc' => 'Ensemble des étudiants du cours',
+                    'members' => [
+                        [
+                            'firstname' => 'Jeannette',
+                            'lastname' => 'Dupont',
+                            'email' => 'jean@dupont.fr',
+                        ],
+                        [
+                            'firstname' => 'Jane',
+                            'lastname' => 'Doe',
+                            'email' => 'john@doe.fr',
+                        ]
+                    ],
+                ],
+            ]
+        ]);
+    }
+}
